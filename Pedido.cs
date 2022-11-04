0ac560356fb67d25757a651b1bcbf3b0cba031b1
HOME <HOME@MATEO1115>
2022-11-04 17:06:46 -0500

    Agregar archivos de proyecto.

---
+++ Pedido.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VentaDeBarrios
+{
+    public class Pedido
+    {
+        //Atributos
+        public string barrio;
+        public int cantidadGalletas;
+
+        //Constructor
+        public Pedido()
+        {
+            barrio = "";
+            cantidadGalletas = 0;
+        }
+
+        //Propiedades
+        public string Barrio
+        {
+            get { return barrio; }
+            set { barrio = value; }
+        }
+
+        public int CantidadGalletas
+        {
+            get { return cantidadGalletas; }
+            set { cantidadGalletas = value; }
+        }
+    }
+}
